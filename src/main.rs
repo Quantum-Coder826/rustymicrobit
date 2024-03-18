d924ad244206cd32a3dacb52484a04bbb21d39e4
Quantum-Coder826 <b.veldthuis@gmail.com>
2024-03-18 18:03:02 +0100

    added possibilaty for gdb debuggin (need to look into this)

--- src/main.rs
+++ src/main.rs
@@ -5,14 +5,15 @@ use cortex_m::asm::nop;
 use cortex_m_rt::entry;
 use panic_halt as _;
 use rtt_target::{rtt_init_print, rprintln};
+
 #[entry]
 fn main() ->!{
     rtt_init_print!();
     rprintln!("Hello, world!");
     loop {
         rprintln!("echo");
-        for _ in 0..100000 {
-            nop();
+        for _ in 0..100_000 {
+            nop()
         }
     }
 }

292ed036eec82dc3c101c267f5dfdaa9cef13d92
Quantum-Coder826 <b.veldthuis@gmail.com>
2024-03-18 17:52:12 +0100

    added rtt debugging

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,18 @@
 #![no_std]
 #![no_main]
 
+use cortex_m::asm::nop;
 use cortex_m_rt::entry;
 use panic_halt as _;
-
+use rtt_target::{rtt_init_print, rprintln};
 #[entry]
 fn main() ->!{
-    loop {}
+    rtt_init_print!();
+    rprintln!("Hello, world!");
+    loop {
+        rprintln!("echo");
+        for _ in 0..100000 {
+            nop();
+        }
+    }
 }
